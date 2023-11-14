09a166336e00469b54ffddc6502a8cde1c28920b
Saba <modan9999@gmail.com>
2023-11-14 20:30:07 +0300

    hw2 t1

--- hw_task_2/hw_2.py
+++ hw_task_2/hw_2.py
@@ -1,1 +1,12 @@
-print('Hello')
+# Задание 1
+def remove_stop_words(input_words, stop_words):
+    words = input_words.split()
+    format_input = list(filter(lambda x: not (x in stop_words), words))
+    return format_input
+
+
+test_string = 'C test Python promt go words php'
+stops = ['Python', 'php', 'go', 'C']
+print(remove_stop_words(test_string, stops))
+
+# Задание 2
